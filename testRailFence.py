44d3a42b0c4ddf168d1600e1549c7cd90bf496c1
Tim Cinel <email@timcinel.com>
2011-03-08 22:08:42 +1100

    Initial commit

---
+++ testRailFence.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+
+from railFence import decryptRailFence
+
+print decryptRailFence("WECRLTEERDSOEEFEAOCAIVDEN", 3, 0); #(reference: http://en.wikipedia.org/wiki/Rail_Fence_Cipher)
+print decryptRailFence("WWHLIAATTEHSDITETH", 7, 0); #(reference: http://i.imgur.com/kWGmM.gif)
